0393294bc259f6d71a82c49994a0b707ba2a335f
daost <daostasik@gmail.com>
2025-02-23 15:43:27 +0500

    init commit

--- utilities.go
+++ utilities.go
@@ -1,11 +1,16 @@
 package s_utils
 
-//func ConfigureSdb(db sdb.Sdb, logger slogger.Logger, log bool, funcName string, traceId string, req interface{}) (slogger.Logger, sdb.Sdb) {
-//	l := logger
-//	l.AddToPrefix("[" + funcName + " Method]")
-//	l.AddToPrefix("[" + traceId + "]")
-//	l.Line()
-//	l.Info(req)
-//
-//	return l, *db.Set(log, l)
-//}
+import (
+	"github.com/daostas/sdb"
+	"github.com/daostas/slogger"
+)
+
+func ConfigureSdb(db sdb.Sdb, logger slogger.Logger, log bool, funcName string, traceId string, req interface{}) (slogger.Logger, sdb.Sdb) {
+	l := logger
+	l.AddToPrefix("[" + funcName + " Method]")
+	l.AddToPrefix("[" + traceId + "]")
+	l.Line()
+	l.Info(req)
+
+	return l, *db.Set(log, l)
+}
